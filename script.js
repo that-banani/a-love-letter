32cb3d6250216237e599425bac58838a307d66bb
Banani <164050878+that-banani@users.noreply.github.com>
2025-11-20 20:23:38 -0300

    second commit

--- script.js
+++ script.js
@@ -36,3 +36,34 @@ function updateLTitle() {
 }
 
 setInterval(updateLTitle, 5000)
+
+
+
+function getTimePassed() {
+    // The exact moment you gave:
+    const start = new Date("2022-10-03T22:00:00");
+    const now = new Date();
+
+    let diff = Math.floor((now - start) / 1000); // difference in seconds
+
+    const years = Math.floor(diff / (3600 * 24 * 365));
+    diff -= years * 3600 * 24 * 365;
+
+    const months = Math.floor(diff / (3600 * 24 * 30.4375)); 
+    diff -= Math.floor(months * 3600 * 24 * 30.4375);
+
+    const days = Math.floor(diff / (3600 * 24));
+    diff -= days * 3600 * 24;
+
+    const hours = Math.floor(diff / 3600);
+    diff -= hours * 3600;
+
+    const minutes = Math.floor(diff / 60);
+    diff -= minutes * 60;
+
+    const seconds = diff;
+
+    document.getElementById('timer').innerHTML = `${years} anos, ${months} meses, ${days} dias, ${hours} horas, ${minutes} minutos e ${seconds} segundos`;
+};
+
+setInterval(getTimePassed, 1000)
